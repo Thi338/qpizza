8202cccd42b8e89c08c1bb18f6816cdc1f2fff38
Thiago Silva <thiago.silca@hotmail.com>
2025-05-04 01:13:16 +0000

    WIP 4

---
+++ qpizza-api/src/main/java/qpizza/InfoResource.java
@@ -0,0 +1,19 @@
+package qpizza;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.Map;
+
+@Path("/info")
+public class InfoResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> getInfo() {
+        var info = Map.of("info", "Ola QPizza");
+        return info;
+    }
+
+}
---
+++ qpizza-api/src/main/java/qpizza/Menu.java
@@ -0,0 +1,10 @@
+package qpizza;
+
+public class Menu {
+
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+}
